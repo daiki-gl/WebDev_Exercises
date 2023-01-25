5e4c3357d70b2cd1774405e5d50cd8bf0595908d
Daiki Yamamoto <0ey329231k1095v@gmail.com>
2023-01-24 20:21:35 -0800

    added delete

--- WebDev2-Exercise2/app.js
+++ WebDev2-Exercise2/app.js
@@ -1,20 +1,32 @@
 //Adding a note
-const addInput = document.getElementById('add-input')
 const addBtn = document.getElementById('add-btn')
-let inputVal
 
-addInput.addEventListener('keyup', (e) => {
-  inputVal = e.target.value
-})
-
-addBtn.addEventListener('click', (e) => {
+const addNote = (e) => {
+  let inputVal = document.getElementById('add-input').value
   e.preventDefault()
   const list = document.getElementById('list')
   const li = document.createElement('li')
   li.innerHTML = `<p>${inputVal}</p>
-  <p>
-    <i class="fa fa-pencil-square-o"></i><i class="fa fa-times"></i
-  </p>
-  <input type="text" class="edit-note">`
+                  <p>
+                    <i class="fa fa-pencil-square-o"></i><i class="fa fa-times"></i
+                    </p>
+                    <input type="text" class="edit-note">`
   list.appendChild(li)
-})
+  inputVal = ''
+}
+addBtn.addEventListener('click', addNote)
+
+// ===================================
+// delete
+// ===================================
+window.addEventListener('load', resetItems)
+addBtn.addEventListener('click', resetItems)
+
+function resetItems() {
+  const deleteBtns = document.querySelectorAll('.fa-times')
+  deleteBtns.forEach((item) =>
+    item.addEventListener('click', (e) =>
+      e.target.parentNode.parentNode.remove()
+    )
+  )
+}
