4cefd7aef6f119bb39a6ded31750402506ec3634
Daiki Yamamoto <0ey329231k1095v@gmail.com>
2023-01-31 11:45:55 -0800

    exercise6

---
+++ WebDev2-Exercise6/script.js
@@ -0,0 +1,41 @@
+/**
+ * Assign a click event handler to the add button with an id of addTask.
+ *
+ * When the add button is clicked:
+ *  - If the textbox is empty, generate an alert with the text “Error: Please enter a task first”.
+ *  - If the text box is not empty, generate an alert with the text containing the task name. For example, if the text in the input box is “Complete Assignment”, generate an alert with text “New Task: Complete Assignment”.
+ *  - Clear the text inside the text box after the add button is clicked.
+ */
+
+$(document).ready(function () {
+  // code goes here
+
+  $('#addTask').on('click', function (e) {
+    const text = $('.textBox').val()
+    if (!text) {
+      alert('Error: Please enter a task first')
+      return
+    } else {
+      alert(`New Task: ${text}`)
+      const newTask = $(
+        `<li class="task">${text}
+            <i class="fa-solid fa-check"></i>
+            <i class="fa-solid fa-trash-can"></i>
+        </li>`
+      )
+      $('.notCompleted').append(newTask)
+    }
+
+    $('.fa-check').click(function () {
+      const checkedTask = $(this).parent()
+      checkedTask.detach()
+      $('.completed').append(checkedTask)
+    })
+
+    $('.fa-trash-can').click(function () {
+      $(this).parent().fadeOut(1000)
+      $(this).parent().remove()
+    })
+    $('.textBox').val('')
+  })
+})
