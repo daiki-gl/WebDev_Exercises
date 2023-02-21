2870dac731c6a1e37625d0fbf69ba3840627aec6
Daiki Yamamoto <0ey329231k1095v@gmail.com>
2023-02-21 11:37:24 -0800

    modified

--- WebDev2-Exercise9/main.js
+++ WebDev2-Exercise9/main.js
@@ -5,16 +5,19 @@ const Timeline = gsap.timeline({
   },
 })
 
+gsap.set('.beer', { y: 10 })
+
 Timeline.from('.logo', { x: -50 })
   .from('li', { x: -50, stagger: 0.1 })
   .from('.title', { y: 50 })
   .from('.tagline', { y: 50 })
   .from('.desc', { y: 50 })
-  .from('.beer', { y: -100, ease: 'power3' })
+  .from('.beer', { y: -100 })
   .to('.beer', {
-    y: -10,
+    y: -15,
     repeat: -1,
     autoAlpha: 1,
     yoyo: true,
     duration: 1,
+    ease: Power1.easeInOut,
   })
