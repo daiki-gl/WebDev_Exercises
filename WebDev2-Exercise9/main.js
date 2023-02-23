7ade87a54cb50c0fec179ead417f0e8954d75522
Daiki Yamamoto <0ey329231k1095v@gmail.com>
2023-02-23 12:30:19 -0800

    added exercise10

--- WebDev2-Exercise9/main.js
+++ WebDev2-Exercise9/main.js
@@ -1,23 +1,26 @@
-const Timeline = gsap.timeline({
-  defaults: {
-    duration: 0.5,
-    autoAlpha: 0,
-  },
-})
-
-gsap.set('.beer', { y: 10 })
-
-Timeline.from('.logo', { x: -50 })
-  .from('li', { x: -50, stagger: 0.1 })
-  .from('.title', { y: 50 })
-  .from('.tagline', { y: 50 })
-  .from('.desc', { y: 50 })
-  .from('.beer', { y: -100 })
-  .to('.beer', {
-    y: -15,
-    repeat: -1,
-    autoAlpha: 1,
-    yoyo: true,
-    duration: 1,
-    ease: Power1.easeInOut,
+window.document.addEventListener('DOMContentLoaded', function () {
+  const Timeline = gsap.timeline({
+    defaults: {
+      duration: 1,
+      autoAlpha: 0,
+    },
   })
+  // gsap.set('.beer', { y: 10 })
+
+  Timeline.from('.logo', { x: -50 })
+    .from('li', { x: -50, stagger: 0.1 })
+    .from('.title', { y: 50 }, '-=0.4')
+    .from('.tagline', { y: 50 }, '-=0.7')
+    .from('.desc', { y: 50 }, '<')
+    .from('.beer', { y: -800 })
+    .to('.beer', {
+      // y: -15,
+      y: '15%',
+      repeat: -1,
+      autoAlpha: 1,
+      yoyo: true,
+      duration: 1,
+      // ease: Power1.easeInOut,
+      ease: 'power1.inOut',
+    })
+})
